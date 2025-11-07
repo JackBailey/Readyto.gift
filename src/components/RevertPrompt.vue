60bb31362b69de2d71e5ebbfbe2e7c3a4ff367fd
JackBailey <81699395+JackBailey@users.noreply.github.com>
2025-11-07 18:21:12 +0000

    Add undo feature to item + list field updating

---
+++ src/components/RevertPrompt.vue
@@ -0,0 +1,28 @@
+<template>
+    <p
+        @click="$emit('revert')"
+        class="d-flex ga-4 align-center font-weight-bold text-surface-variant cursor-pointer"
+    >
+        <v-icon
+            size="x-small"
+            color="primary"
+        >
+            {{ mdiUndoVariant }}
+        </v-icon>
+        <span>
+            Revert to <span class="text-primary">{{ previousValue }}</span>
+        </span>
+    </p>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { mdiUndoVariant } from "@mdi/js";
+
+defineProps({
+    previousValue: {
+        required: true,
+        type: [String, Number, Object, Array]
+    }
+});
+</script>
